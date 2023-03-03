c50afb850c6a0573f68df79352939ad343a9ed82
chenyqmy <chenyq_my@163.com>
2023-03-03 15:22:48 +0800

    first commit

---
+++ 10print.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import random, time
+
+root = tk.Tk()
+root.title("10Print")
+
+canvas = tk.Canvas(root, width=600, height=600)
+canvas.pack()
+
+def draw_line(x, y, size):
+    if random.random() > 0.5:
+        canvas.create_line(x, y, x+size, y+size)
+    else:
+        canvas.create_line(x, y+size, x+size, y)
+    time.sleep(0.005)
+size = 20
+y_offset = 0
+
+for y in range(0, 600, size):
+    for x in range(0, 600, size):
+        draw_line(x, y, size)
+        canvas.update()
+        print(x,y)
+        
+
+if x == 580 and y == 580:
+    canvas.move("all",0,-size)
+    while True:
+        for x in range(0, 600, size):
+            draw_line(x, 580, size)
+            canvas.update()
+        canvas.move("all",0,-size)
+
+
+root.mainloop()
